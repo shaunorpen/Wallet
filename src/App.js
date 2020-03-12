b8a7fdc3a6355a637b7c857e536dc41253368b30
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-12 16:13:59 +0000

    add axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
-import data from './data'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import data from './data';
 import './App.css';
 
 function App() {
   let [user, setUser] = useState('Brian');
   let [blockchain, setBlockchain] = useState(data);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/chain')
+      .then(res => {
+        setBlockchain(res.data.length > 1 ? res.data : data);
+      })
+      .catch(err => {
+        alert(err.message);
+      })
+  }, [])
 
   function extractTransactions(blockchain) {
     const transactions = [];
