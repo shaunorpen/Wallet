5950f12f24364ddb3d1652ea1f082f9c3d97b873
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-12 15:23:55 +0000

    add user dropdown

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import blockchain from './data'
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState('Beej');
+  let [user, setUser] = useState('Brian');
 
   function extractTransactions(blockchain) {
     const transactions = [];
@@ -33,15 +33,31 @@ function App() {
 
   const transactions = extractTransactions(blockchain);
 
+  const handleChange = e => {
+    setUser(e.target.value);
+  }
+
   return (
     <div className="App">
       <h2>Wallet</h2>
       {/* * Allow the user to enter, save, or change the `id` used for the program */}
+      <select name="user" id="user" value={user} onChange={handleChange} >
+        <option value="Brian">Brian</option>
+        <option value="Beej">Beej</option>
+        <option value="Brady">Brady</option>
+        <option value="Elissa">Elissa</option>
+        <option value="Tom">Tom</option>
+      </select>
       <h2>Balance</h2>
       {/* * Display the current balance for that user */}
       <p>{user} has a balance of LC{caculateBalance(transactions, user)}</p>
       <h2>Transactions</h2>
       {/* * Display a list of all transactions for this user, including sender and recipient */}
+      <div style={{ display:"flex", justifyContent:"space-around", fontWeight:"bold" }}>
+          <div>Sender</div>
+          <div>Recipient</div>
+          <div>Amount</div>
+        </div>
       <div>
         <Transactions transactions={transactions.filter(transaction => transaction.sender === user || transaction.recipient === user)} user={user} />
       </div>
@@ -52,8 +68,8 @@ function App() {
 function Transactions({ transactions, user }) {
   return transactions
     .map(transaction => (
-        <div style={{ display:"flex", justifyContent:"space-around"}}>
-          <div>{transaction.sender}</div>
+        <div style={{ display:"flex", justifyContent:"space-around" }}>
+          <div>{transaction.sender === '0' ? 'Block reward' : transaction.sender }</div>
           <div>{transaction.recipient}</div>
           <div>LC{transaction.amount}</div>
         </div>
